e8b78d950e91f8c55a1a8170346281feb261daf5
Lucas Carvalho <lucas.carvalhocco@gmail.com>
2020-03-09 20:11:31 -0300

    create card projects

--- lib/pages/pageTwo/PageTwo.dart
+++ lib/pages/pageTwo/PageTwo.dart
@@ -11,28 +11,165 @@ class _PageTwoState extends State<PageTwo> {
     return Scaffold(
       body: Container(
         alignment: AlignmentDirectional.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[cardProjet()],
+        child: SafeArea(
+          child: ListView(
+            padding: const EdgeInsets.all(10.0),
+            children: <Widget>[
+              Card(
+                child: CustomListItemTwo(
+                  thumbnail: Container(
+                    decoration: const BoxDecoration(color: Colors.green),
+                  ),
+                  title: 'projeto 1',
+                  subtitle:
+                      'Flutter continues to improve and expand its horizons.'
+                      'This text should max out at two lines and clip',
+                  author: 'Lucas Carvalho',
+                  publishDate: 'Jan 13',
+                  readDuration: '300 mins',
+                ),
+              ),
+              Card(
+                child: CustomListItemTwo(
+                  thumbnail: Container(
+                    decoration: const BoxDecoration(color: Colors.greenAccent),
+                  ),
+                  title: 'projeto 2',
+                  subtitle: 'Flutter once again improves and makes updates.',
+                  author: 'Lucas Carvalho',
+                  publishDate: 'Feb 26',
+                  readDuration: '520 mins',
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-Widget cardProjet() {
-  return Padding(
-    padding: const EdgeInsets.all(20.0),
-    child: Row(
+class _ArticleDescription extends StatelessWidget {
+  _ArticleDescription({
+    Key key,
+    this.title,
+    this.subtitle,
+    this.author,
+    this.publishDate,
+    this.readDuration,
+  }) : super(key: key);
+
+  final String title;
+  final String subtitle;
+  final String author;
+  final String publishDate;
+  final String readDuration;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        Container(
-          alignment: AlignmentDirectional.center,
-          height: 120,
-          width: 120,
-          color: Colors.green,
-          child: Text('Project Image'),
+        Expanded(
+          flex: 2,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                '$title',
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const Padding(padding: EdgeInsets.only(bottom: 2.0)),
+              Text(
+                '$subtitle',
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  fontSize: 12.0,
+                  color: Colors.black54,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Text(
+                '$author',
+                style: const TextStyle(
+                  fontSize: 12.0,
+                  color: Colors.black87,
+                ),
+              ),
+              Text(
+                '$publishDate · $readDuration ★',
+                style: const TextStyle(
+                  fontSize: 12.0,
+                  color: Colors.black54,
+                ),
+              ),
+            ],
+          ),
         ),
       ],
-    ),
-  );
+    );
+  }
+}
+
+class CustomListItemTwo extends StatelessWidget {
+  CustomListItemTwo({
+    Key key,
+    this.thumbnail,
+    this.title,
+    this.subtitle,
+    this.author,
+    this.publishDate,
+    this.readDuration,
+  }) : super(key: key);
+
+  final Widget thumbnail;
+  final String title;
+  final String subtitle;
+  final String author;
+  final String publishDate;
+  final String readDuration;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0),
+      child: SizedBox(
+        height: 100,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 1.0,
+              child: thumbnail,
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(20.0, 0.0, 2.0, 0.0),
+                child: _ArticleDescription(
+                  title: title,
+                  subtitle: subtitle,
+                  author: author,
+                  publishDate: publishDate,
+                  readDuration: readDuration,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
